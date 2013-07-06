57af56c0b82513cb74aa32362d0a887ef5ab6a61
Coursera Startup Engineering <c.se@thelovelynerds.com>
2013-07-06 05:39:12 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync("index.html", "utf8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
